25bb14a8a2c3f4c24144ff10bf919a2c4bde0737
gabrielmamani <mamanigabriel2014@gmail.com>
2017-04-12 21:51:58 -0400

    ip publica servidor de gabriel timer ok

--- app/src/main/java/info/androidhive/volleyexamples/JsonRequestActivity.java
+++ app/src/main/java/info/androidhive/volleyexamples/JsonRequestActivity.java
@@ -5,6 +5,8 @@ import info.androidhive.volleyexamples.utils.Const;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,202 +33,210 @@ import com.android.volley.toolbox.JsonObjectRequest;
 
 public class JsonRequestActivity extends Activity implements OnClickListener {
 
-	private String TAG = JsonRequestActivity.class.getSimpleName();
-	private Button btnJsonObj, btnJsonArray;
-	private TextView msgResponse;
-	private ProgressDialog pDialog;
-	public String jsonResponse;
+    private String TAG = JsonRequestActivity.class.getSimpleName();
+    private Button btnJsonObj, btnJsonArray;
+    private TextView msgResponse;
+    private ProgressDialog pDialog;
+    public String jsonResponse;
 
-	// These tags will be used to cancel the requests
-	private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
+    // These tags will be used to cancel the requests
+    private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_json);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json);
 
-		btnJsonObj = (Button) findViewById(R.id.btnJsonObj);
-		btnJsonArray = (Button) findViewById(R.id.btnJsonArray);
-		msgResponse = (TextView) findViewById(R.id.msgResponse);
+        btnJsonObj = (Button) findViewById(R.id.btnJsonObj);
+        btnJsonArray = (Button) findViewById(R.id.btnJsonArray);
+        msgResponse = (TextView) findViewById(R.id.msgResponse);
 
-		pDialog = new ProgressDialog(this);
-		pDialog.setMessage("Loading...");
-		pDialog.setCancelable(false);
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
 
-		btnJsonObj.setOnClickListener(this);
-		btnJsonArray.setOnClickListener(this);
-	}
+        btnJsonObj.setOnClickListener(this);
+        btnJsonArray.setOnClickListener(this);
+    }
 
-	private void showProgressDialog() {
-		if (!pDialog.isShowing())
-			pDialog.show();
-	}
+    private void showProgressDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
 
-	private void hideProgressDialog() {
-		if (pDialog.isShowing())
-			pDialog.hide();
-	}
+    private void hideProgressDialog() {
+        if (pDialog.isShowing())
+            pDialog.hide();
+    }
 
-	/**
-	 * Making json object request
-	 * */
-	private void makeJsonObjReq() {
-		showProgressDialog();
-		JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
-				Const.URL_JSON_OBJECT, null,
-				new Response.Listener<JSONObject>() {
+    /**
+     * Making json object request
+     */
+    private void makeJsonObjReq() {
+        showProgressDialog();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+                Const.URL_JSON_OBJECT, null,
+                new Response.Listener<JSONObject>() {
 
-					@Override
-					public void onResponse(JSONObject response) {
-						Log.d(TAG, response.toString());
-						msgResponse.setText(response.toString());
-						hideProgressDialog();
-					}
-				}, new Response.ErrorListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        msgResponse.setText(response.toString());
+                        hideProgressDialog();
+                    }
+                }, new Response.ErrorListener() {
 
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						VolleyLog.d(TAG, "Error: " + error.getMessage());
-						hideProgressDialog();
-					}
-				}) {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+            }
+        }) {
 
-			/**
-			 * Passing some request headers
-			 * */
-			@Override
-			public Map<String, String> getHeaders() throws AuthFailureError {
-				HashMap<String, String> headers = new HashMap<String, String>();
-				headers.put("Content-Type", "application/json");
-				return headers;
-			}
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
 
-			@Override
-			protected Map<String, String> getParams() {
-				Map<String, String> params = new HashMap<String, String>();
-				params.put("placa", "todas");
-				//params.put("email", "abc@androidhive.info");
-				//params.put("pass", "password123");
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("placa", "todas");
+                //params.put("email", "abc@androidhive.info");
+                //params.put("pass", "password123");
 
-				return params;
-			}
+                return params;
+            }
 
-		};
+        };
 
-		// Adding request to request queue
-		AppController.getInstance().addToRequestQueue(jsonObjReq,
-				tag_json_obj);
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq,
+                tag_json_obj);
 
-		// Cancelling request
-		// ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_obj);		
-	}
+        // Cancelling request
+        // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_obj);
+    }
 
-	/**
-	 * Making json array request
-	 * */
-	private void makeJsonArryReq() {
-		showProgressDialog();
-		JsonArrayRequest req = new JsonArrayRequest(Const.URL_JSON_ARRAY,
-				new Response.Listener<JSONArray>() {
-					@Override
-					public void onResponse(JSONArray response) {
-						Log.d(TAG, response.toString());
-						//msgResponse.setMovementMethod(new ScrollingMovementMethod());
-						//msgResponse.setText(response.toString());
-						//hideProgressDialog();
+    /**
+     * Making json array request
+     */
+    private void makeJsonArryReq() {
+        showProgressDialog();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
 
-						try {
-							// Parsing json array response
-							// loop through each json object
-							jsonResponse = "";
-							for (int i = 0; i < response.length(); i++) {
+                JsonArrayRequest req = new JsonArrayRequest(Const.URL_JSON_ARRAY,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                Log.d(TAG, response.toString());
+                                //msgResponse.setMovementMethod(new ScrollingMovementMethod());
+                                //msgResponse.setText(response.toString());
+                                //hideProgressDialog();
 
-								JSONObject trama = (JSONObject) response.get(i);
+                                try {
+                                    // Parsing json array response
+                                    // loop through each json object
+                                    jsonResponse = "";
+                                    for (int i = 0; i < response.length(); i++) {
 
-								String asimut = trama.getString("Asimut");
-								String EstadoGPS = trama.getString("EstadoGPS");
-								String EstadoMotor = trama.getString("EstadoMotor");
-								String EstadoPuerta = trama.getString("EstadoPuerta");
-								String FechaGPS = trama.getString("FechaGPS");
-								String ID = trama.getString("ID");
-								String IDButton = trama.getString("IDButton");
-								String IMEI = trama.getString("IMEI");
-								String Kilometraje = trama.getString("Kilometraje");
-								String Latitud = trama.getString("Latitud");
-								String Longitud = trama.getString("Longitud");
-								String NIT = trama.getString("NIT");
-								String Nombre = trama.getString("Nombre");
-								String Nro = trama.getString("Nro");
-								String NroPlaca = trama.getString("NroPlaca");
-								String Temperatura = trama.getString("Temperatura");
-								String Velocidad = trama.getString("Velocidad");
-								String VoltajeBateria = trama.getString("VoltajeBateria");
-								String direcciones = trama.getString("direcciones");
-								String tipov = trama.getString("tipov");
+                                        JSONObject trama = (JSONObject) response.get(i);
 
-								jsonResponse += "asimut: " + asimut + "\n";
-								jsonResponse += "EstadoGPS: " + EstadoGPS + "\n";
-								jsonResponse += "EstadoMotor: " + EstadoMotor + "\n";
-								jsonResponse += "EstadoPuerta: " + EstadoPuerta + "\n";
-								jsonResponse += "FechaGPS: " + FechaGPS + "\n";
-								jsonResponse += "ID: " + ID + "\n";
-								jsonResponse += "IDButton: " + IDButton + "\n";
-								jsonResponse += "IMEI: " + IMEI + "\n";
-								jsonResponse += "Kilometraje: " + Kilometraje + "\n";
-								jsonResponse += "Latitud: " + Latitud + "\n";
-								jsonResponse += "Longitud: " + Longitud + "\n";
-								jsonResponse += "NIT: " + NIT + "\n";
-								jsonResponse += "Nombre: " + Nombre + "\n";
-								jsonResponse += "Nro: " + Nro + "\n";
-								jsonResponse += "NroPlaca: " + NroPlaca + "\n";
-								jsonResponse += "Temperatura: " + Temperatura + "\n";
-								jsonResponse += "Velocidad: " + Velocidad + "\n";
-								jsonResponse += "VoltajeBateria: " + VoltajeBateria + "\n";
-								jsonResponse += "direcciones: " + direcciones + "\n";
-								jsonResponse += "tipov: " + tipov + "\n\n\n";
-							}
+                                        String asimut = trama.getString("Asimut");
+                                        String EstadoGPS = trama.getString("EstadoGPS");
+                                        String EstadoMotor = trama.getString("EstadoMotor");
+                                        String EstadoPuerta = trama.getString("EstadoPuerta");
+                                        String FechaGPS = trama.getString("FechaGPS");
+                                        String ID = trama.getString("ID");
+                                        String IDButton = trama.getString("IDButton");
+                                        String IMEI = trama.getString("IMEI");
+                                        String Kilometraje = trama.getString("Kilometraje");
+                                        String Latitud = trama.getString("Latitud");
+                                        String Longitud = trama.getString("Longitud");
+                                        String NIT = trama.getString("NIT");
+                                        String Nombre = trama.getString("Nombre");
+                                        String Nro = trama.getString("Nro");
+                                        String NroPlaca = trama.getString("NroPlaca");
+                                        String Temperatura = trama.getString("Temperatura");
+                                        String Velocidad = trama.getString("Velocidad");
+                                        String VoltajeBateria = trama.getString("VoltajeBateria");
+                                        String direcciones = trama.getString("direcciones");
+                                        String tipov = trama.getString("tipov");
 
-							msgResponse.setMovementMethod(new ScrollingMovementMethod());
-							msgResponse.setText(jsonResponse);
+                                        jsonResponse += "asimut: " + asimut + "\n";
+                                        jsonResponse += "EstadoGPS: " + EstadoGPS + "\n";
+                                        jsonResponse += "EstadoMotor: " + EstadoMotor + "\n";
+                                        jsonResponse += "EstadoPuerta: " + EstadoPuerta + "\n";
+                                        jsonResponse += "FechaGPS: " + FechaGPS + "\n";
+                                        jsonResponse += "ID: " + ID + "\n";
+                                        jsonResponse += "IDButton: " + IDButton + "\n";
+                                        jsonResponse += "IMEI: " + IMEI + "\n";
+                                        jsonResponse += "Kilometraje: " + Kilometraje + "\n";
+                                        jsonResponse += "Latitud: " + Latitud + "\n";
+                                        jsonResponse += "Longitud: " + Longitud + "\n";
+                                        jsonResponse += "NIT: " + NIT + "\n";
+                                        jsonResponse += "Nombre: " + Nombre + "\n";
+                                        jsonResponse += "Nro: " + Nro + "\n";
+                                        jsonResponse += "NroPlaca: " + NroPlaca + "\n";
+                                        jsonResponse += "Temperatura: " + Temperatura + "\n";
+                                        jsonResponse += "Velocidad: " + Velocidad + "\n";
+                                        jsonResponse += "VoltajeBateria: " + VoltajeBateria + "\n";
+                                        jsonResponse += "direcciones: " + direcciones + "\n";
+                                        jsonResponse += "tipov: " + tipov + "\n\n\n";
+                                    }
 
-							hideProgressDialog();
+                                    msgResponse.setMovementMethod(new ScrollingMovementMethod());
+                                    msgResponse.setText(jsonResponse);
 
-						} catch (JSONException e) {
-							e.printStackTrace();
-							Toast.makeText(getApplicationContext(),
-									"Error: " + e.getMessage(),
-									Toast.LENGTH_LONG).show();
-						}
+                                    hideProgressDialog();
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                    Toast.makeText(getApplicationContext(),
+                                            "Error: " + e.getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
 
 
-					}
-				}, new Response.ErrorListener() {
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						VolleyLog.d(TAG, "Error: " + error.getMessage());
-						hideProgressDialog();
-					}
-				});
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                        hideProgressDialog();
+                    }
+                });
 
-		// Adding request to request queue
-		AppController.getInstance().addToRequestQueue(req,
-				tag_json_arry);
+                // Adding request to request queue
+                AppController.getInstance().addToRequestQueue(req,
+                        tag_json_arry);
 
-		// Cancelling request
-		// ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_arry);
-	}
+                // Cancelling request
+                // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_arry);
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.btnJsonObj:
-			makeJsonObjReq();
-			break;
-		case R.id.btnJsonArray:
-			makeJsonArryReq();
-			break;
-		}
+            }
+        }, 0, 5000);
+    }
 
-	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnJsonObj:
+                makeJsonObjReq();
+                break;
+            case R.id.btnJsonArray:
+                makeJsonArryReq();
+                break;
+        }
+
+    }
 
 }
--- app/src/main/java/info/androidhive/volleyexamples/utils/Const.java
+++ app/src/main/java/info/androidhive/volleyexamples/utils/Const.java
@@ -2,7 +2,7 @@ package info.androidhive.volleyexamples.utils;
 
 public class Const {
 	public static final String URL_JSON_OBJECT = "http://190.186.39.180:8000/api/products";
-	public static final String URL_JSON_ARRAY = "http://190.186.39.180:8000/api/tramas";
+	public static final String URL_JSON_ARRAY = "http://192.168.2.243:8000/api/tramas";
 	public static final String URL_STRING_REQ = "http://192.168.1.138:8090/WebServices/WisetrackServices.asmx/HelloWorld";
 	public static final String URL_IMAGE = "http://api.androidhive.info/volley/volley-image.jpg";
 }
